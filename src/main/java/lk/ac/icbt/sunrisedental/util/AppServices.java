aa855d07a931189d2aa3119309eed3d923ac52a1
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2026-08-23 13:09:14 +0530

    Wire application services

---
+++ src/main/java/lk/ac/icbt/sunrisedental/util/AppServices.java
@@ -0,0 +1,15 @@
+package lk.ac.icbt.sunrisedental.util;
+
+import lk.ac.icbt.sunrisedental.dao.*;
+import lk.ac.icbt.sunrisedental.service.*;
+import java.math.BigDecimal;
+
+public final class AppServices {
+    private static final AppointmentService APPOINTMENTS = new AppointmentService(new JdbcAppointmentDao(), new JdbcCatalogDao());
+    private static final AuthenticationService AUTH = new AuthenticationService(new JdbcUserDao());
+    private static final BillingService BILLING = new BillingService(APPOINTMENTS, new JdbcBillDao(), new BigDecimal("1000.00"));
+    private AppServices() { }
+    public static AppointmentService appointments() { return APPOINTMENTS; }
+    public static AuthenticationService authentication() { return AUTH; }
+    public static BillingService billing() { return BILLING; }
+}
